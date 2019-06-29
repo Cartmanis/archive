96442e5d45ba54b39085f2d08d1dddf4d63f8245
cartmani <VShmelcer@gmail.com>
2019-06-29 12:18:30 +0700

    реализован UnZipPath

--- fzip/zip_reader.go
+++ fzip/zip_reader.go
@@ -1,1 +1,37 @@
 package fzip
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func UnZipPath(zipFile string) error {
+	r, err := zip.OpenReader(zipFile)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if err := os.MkdirAll(filepath.Dir(f.Name), 0766); err != nil {
+			return err
+		}
+		nf, err := os.OpenFile(f.Name, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer nf.Close()
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+		_, err = io.Copy(nf, rc)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
